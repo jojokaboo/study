cc22c98f6b65aaecb5f71fe337d3958f2e5f473f
Мария <mrs.seliv.0111@gmail.com>
2023-07-14 19:12:55 +0300

    completed increment feature

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def print_hi(name):
     return name + "alvarez"  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def inc(number):
+    return number + 1
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
